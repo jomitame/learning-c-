6558691d3473164e70c43e735af376b0d3374efd
jomitame <jomitame@gmail.com>
2023-01-31 19:11:44 -0500

    basic for a simple application

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// iostraem library importation
+// include so many functionality as input and outputs
+
+// namespace use
+// for easy use of importations and avoid names conflicts
+using namespace std;
+
+// simple function
+int main(){
+    // printing out
+    cout << "Hello world";
+}
